70929243521859f7b8bccc1471ad27261970ee74
APresekal <presekal@ui.ac.id>
2020-11-23 07:27:16 -0500

    Add files via upload

--- testpg.py
+++ testpg.py
@@ -1,8 +1,23 @@
 import psycopg2
  
 conn = psycopg2.connect(host="131.180.165.5",database="crpg", user="postgres", password="crpg")
- 
+
+#Check Connection
+"""
 if conn is not None:
     print('Connection established to PostgreSQL.')
 else:
     print('Connection not established to PostgreSQL.')
+"""
+
+#Setting auto commit 
+conn.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+# Preparing SQL queries to INSERT a record into the database.
+cursor.execute('''INSERT INTO test(val) VALUES (33)''')
+
+cursor.close()
+conn.close()
