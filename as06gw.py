ee5f36ec43ae5f363f632433e60b031a3dcbbf5b
APresekal <presekal@ui.ac.id>
2020-11-24 08:02:11 -0500

    Add files via upload

--- as06gw.py
+++ as06gw.py
@@ -6,6 +6,7 @@ import psycopg2
 
 
 HOST1 = '100.6.0.11'
+HOST1 = '100.6.0.12'
 PORT1 = 993
 PORT2 = 994
 PORTS1 = 881
@@ -45,11 +46,39 @@ def serverOne():
 
 			print(a)
 
+def serverTwo():
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sc1:
+		sc1.connect((HOST2, PORT1))
+		
+		x = 1
+		while x < 6:
+			#recive data from server A
+			data1 = sc1.recv(1024)
+
+			strval1 = str(data1.decode("utf-8"))
+
+			a,b,c,d,e,f,g = strval1.split("+")
+
+			inserted_values = (
+        		a,
+        		b,
+        		c,
+        		d,
+        		e,
+        		f,
+        		g
+    		)
+
+			cursor.execute(" INSERT INTO s06m2(dtime, cb_ctrl, cb_res, i_res, p_res, q_res, v_res) VALUES (%s,%s,%s,%s,%s,%s,%s)", inserted_values)
+
+			print(b)
+
 
 
 # Create two threads as follows
 try:
    _thread.start_new_thread( serverOne, ( ) )
+   _thread.start_new_thread( serverTwo, ( ) )
 
 except:
    print ("Error: unable to start thread")
